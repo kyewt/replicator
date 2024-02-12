4a261f2a78801da2e4bf5d83b40a75f6b8dee1e5
kyewt <147393851+kyewt@users.noreply.github.com>
2024-02-12 00:23:33 -0600

    Update netIdentityRegister.lua
    Remove secondaryArray related features. 
    Add destructor.
    Rename certain members to reflect new feature set.

--- netIdentityRegister.lua
+++ netIdentityRegister.lua
@@ -1,5 +1,6 @@
-local isPosInt = _G.qtype.isPosInt
+local isPosInt= _G.qtype.isPosInt
 local fixedSparseArray = _G.classes.fixedSparseArray
+local sparseArray = _G.classes.sparseArray
 local insert = table.insert
 
 local netIdentityRegister = _G.qclass.extend("netIdentityRegister")
@@ -7,89 +8,54 @@ local netIdentityRegister = _G.qclass.extend("netIdentityRegister")
 netIdentityRegister:setStaticField("_placeholder", {}, true)
 
 netIdentityRegister:setField("_mainArray", nil, true)
-netIdentityRegister:setField("_secondaryArrays", nil, true)
 netIdentityRegister:setField("_defaultArraySize", 2000000)
 
-netIdentityRegister:setMethod("createSecondaryArray", function(inst, size)
-    if size ~= nil then
-        if not isPosInt(size) then error("bad type to size expected positive integer") end
-    else
-        size = inst._defaultArraySize
-    end
-    local secondaryArray = fixedSparseArray(size)
-    return inst._secondaryArrays.insert(secondaryArray)
-end)
-netIdentityRegister:setMethod("deleteSecondaryArray", function(inst, index)
-    local removed = inst._secondaryArrays.remove(index)
-    if not removed then
-        error("secondaryArray did not exist at index "..tostring(index))
-    end
+netIdentityRegister:setField("destroying", nil, true)
+
+netIdentityRegister:setMethod("getValue", function(inst, index)
+    return inst._mainArray.get(index)
 end)
 
-netIdentityRegister:setMethod("getValue", function(inst, index, secondaryArrayIndex)
-    local value = inst._mainArray.get(index)
-    if not value and secondaryArrayIndex then
-        value = inst._secondaryArrays.get(secondaryArrayIndex).get(index)
-    end
-    return value
-end)
-
-netIdentityRegister:setMethod("getValues", function(inst, indices, secondaryArrayIndex)
+netIdentityRegister:setMethod("getValues", function(inst, indices)
     local array = inst._mainArray
     local values = {}
-    local value = array.get(indices[1])
-    if value then
-        insert(values, value)
-        for i = 2, #indices do
-            value = array.get(indices[i])
-            insert(values, value)
-        end
-    else
-        array = inst._secondaryArrays.get(secondaryArrayIndex)
-        if not array then
-            error("secondaryArray does not exist at index "..secondaryArrayIndex)
-        end
-        for _, index in ipairs(indices) do
-            value = array.get(index)
-            insert(values, value)
-        end
-    end
-    if #values ~= #indices then
-        error("some values were nil")
+    for _, index in ipairs(indices) do
+        insert(values, array.get(index))
     end
     return values
 end)
 
-netIdentityRegister:setMethod("getIndex", function(inst, value, secondaryArrayIndex)
-    local index = inst._mainArray.find(value)
-    if index or not secondaryArrayIndex then
-        return index
+netIdentityRegister:setMethod("getPairsFromValues", function(inst, values)
+    local foundIndices, foundValues = {}, {}
+    local array = inst._mainArray
+    for _, value in ipairs(values) do
+        local index = array.find(value)
+        insert(foundIndices, index)
+        insert(foundValues, value)
     end
-    return inst._secondaryArrays.get(secondaryArrayIndex).find(value)
+    return foundIndices, foundValues
 end)
 
-netIdentityRegister:setMethod("getIndices", function(inst, values, secondaryArrayIndex)
-    local array = inst._mainArray
+netIdentityRegister:setMethod("getAllValuesWithIndices", function(inst)
+    return inst._mainArray.getAllValuesWithIndices()
+end)
+
+netIdentityRegister:setMethod("getAllIndicesWithoutHoles", function(inst)
+    return inst._mainArray.getAllIndicesWithoutHoles()
+end)
+
+netIdentityRegister:setMethod("getIndex", function(inst, value)
+    return inst._mainArray.find(value)
+end)
+
+netIdentityRegister:setMethod("getIndices", function(inst, values)
     local indices = {}
-    local index = array.find(values[1])
-    if index then
-        insert(indices, index)
-        for i = 2, #values do
-            index = array.find(values[i])
+    local array = inst._mainArray
+    for _, value in ipairs(values) do
+        local index = array.find(value)
+        if index then
             insert(indices, index)
         end
-    else
-        array = inst._secondaryArrays.get(secondaryArrayIndex)
-        if not array then
-            error("secondaryArray does not exist at index "..secondaryArrayIndex)
-        end
-        for _, value in ipairs(values) do
-            index = array.find(value)
-            insert(indices, index)
-        end
-    end
-    if #indices ~= #values then
-        error("some indices were nil")
     end
     return indices
 end)
@@ -102,71 +68,47 @@ netIdentityRegister:setMethod("registerValues", function(inst, values)
     return inst._mainArray.insertMul(values)
 end)
 
+netIdentityRegister:setMethod("registerValueAtIndex", function(inst, value, index)
+    inst._mainArray.set(index, value)
+end)
+
+netIdentityRegister:setMethod("registerValuesAtIndices", function(inst, values, indices)
+    if #values ~= #indices then
+        error("size of values and indices is different")
+    end
+    local array = inst._mainArray
+    for i = 1, #values do
+        array.set(indices[i], values[i])
+    end
+end)
+
 netIdentityRegister:setMethod("deregisterValue", function(inst, index)
-    local removed = inst._main.remove(index)
+    local removed = inst._mainArray.remove(index)
     if not removed then
         error("value did not exist at index "..tostring(index))
     end
 end)
 
-netIdentityRegister:setMethod("deregisterValues", function(inst, indices)
-    local removed = inst._main.removeMul(indices)
+netIdentityRegister:setMethod("deregister", function(inst, indices)
+    local removed = inst._mainArray.removeMul(indices)
     if #removed ~= #indices then
-        for _, removedIndex in ipairs(removed) do
-            table.remove(indices, table.find(indices, removedIndex))
-        end
-        error("values did not exist at indices "..indices)
+        error("values did not exist at some indices")
     end
 end)
 
-netIdentityRegister:setMethod("registerSecondaryValue", function(inst, arrayIndex, value)
-    return inst._secondaryArrays.get(arrayIndex).insert(value)
-end)
-
-netIdentityRegister:setMethod("registerSecondaryValues", function(inst, arrayIndex, values)
-    return inst._secondaryArrays.get(arrayIndex).insertMul(values)
-end)
-
-netIdentityRegister:setMethod("deregisterSecondaryValue", function(inst, arrayIndex, index)
-    local array = inst._secondaryArrays.get(arrayIndex)
-    if not array then
-        error("secondaryArray does not exist at index "..arrayIndex)
-    end
-    local removed = array.remove(index)
-    if not removed then
-        error("value did not exist at index "..tostring(index))
-    end
-end)
-
-netIdentityRegister:setMethod("deregisterSecondaryValues", function(inst, arrayIndex, indices)
-    local array = inst._secondaryArrays.get(arrayIndex)
-    if not array then
-        error("secondaryArray does not exist at index "..arrayIndex)
-    end
-    local removed = array.removeMul(indices)
-    if #removed ~= #indices then
-        for _, removedIndex in ipairs(removed) do
-            table.remove(indices, table.find(indices, removedIndex))
-        end
-        error("values did not exist at indices "..indices)
-    end
-end)
-
-netIdentityRegister:setConstructor(function(inst, size, maximumSecondaryArrays)
+netIdentityRegister:setConstructor(function(inst, size)
     if size ~= nil then
         if not isPosInt(size) then error("bad type to size expected positive integer") end
     else
         size = inst._defaultArraySize
     end
-    if maximumSecondaryArrays ~= nil then
-        if not isPosInt(maximumSecondaryArrays) then
-            error("bad type to maximumSecondaryArrays expected positive integer")
-        end
-    else
-        maximumSecondaryArrays = 12
-    end
+    inst.destroying = Instance.new("BindableEvent")
     inst._mainArray = fixedSparseArray(size)
-    inst._secondaryArrays = fixedSparseArray(maximumSecondaryArrays)
+    inst._secondaryArrays = sparseArray()
+end)
+
+netIdentityRegister:setDestructor(function(inst)
+    inst.destroying:Fire()
 end)
 
 netIdentityRegister:finalize()
